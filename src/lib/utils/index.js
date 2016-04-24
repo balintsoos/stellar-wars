837d99461379a0cc7f27ffc48c9697193e31c627
Balint Soos <soosbalint95@gmail.com>
2016-04-25 01:45:18 +0200

    rewrite utils to arrow functions

--- src/lib/utils/index.js
+++ src/lib/utils/index.js
@@ -1,12 +1,8 @@
 //  Utility functions
 
-function decreaseBy(number, by, minimum = 0) {
-  return Math.max(minimum, number - by)
-}
+const decreaseBy = (number, by, minimum = 0) => Math.max(minimum, number - by)
 
-function increaseBy(number, by, maximum = 100) {
-  return Math.min(maximum, number + by)
-}
+const increaseBy = (number, by, maximum = 100) => Math.min(maximum, number + by)
 
 export default {
   decreaseBy,
